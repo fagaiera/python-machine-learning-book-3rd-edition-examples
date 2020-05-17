eae2038908edcdf2f2160eb6bbb95cc3ec8b5d29
Fabio Gaiera <fagaiera@gmail.com>
2020-05-17 23:12:40 +0200

    Chapter 13

---
+++ Chapter 13/03 Building a NN model in TensorFlow/program.py
@@ -0,0 +1,251 @@
+# Building a neural network model in TensorFlow
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow_datasets as tfds
+
+# Building a linear regression model
+X_train = np.arange(10).reshape((10, 1))
+y_train = np.array([1.0, 1.3, 3.1,
+                    2.0, 5.0, 6.3,
+                    6.6, 7.4, 8.0,
+                    9.0])
+
+plt.plot(X_train, y_train, 'o', markersize=10)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+X_train_norm = (X_train - np.mean(X_train))/np.std(X_train)
+
+ds_train_orig = tf.data.Dataset.from_tensor_slices(
+    (tf.cast(X_train_norm, tf.float32),
+     tf.cast(y_train, tf.float32)))
+
+class MyModel(tf.keras.Model):
+    
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.w = tf.Variable(0.0, name='weight')
+        self.b = tf.Variable(0.0, name='bias')
+
+    def call(self, x):
+        return self.w*x + self.b
+
+
+model = MyModel()
+
+model.build(input_shape=(None, 1))
+model.summary()
+
+def loss_fn(y_true, y_pred):
+    return tf.reduce_mean(tf.square(y_true - y_pred))
+
+## testing the function:
+yt = tf.convert_to_tensor([1.0])
+yp = tf.convert_to_tensor([1.5])
+
+loss_fn(yt, yp)
+
+def train(model, inputs, outputs, learning_rate):
+    with tf.GradientTape() as tape:
+        current_loss = loss_fn(model(inputs), outputs)
+    dW, db = tape.gradient(current_loss, [model.w, model.b])
+    model.w.assign_sub(learning_rate * dW)
+    model.b.assign_sub(learning_rate * db)
+
+tf.random.set_seed(1)
+
+num_epochs = 200
+log_steps = 100
+learning_rate = 0.001
+batch_size = 1
+steps_per_epoch = int(np.ceil(len(y_train) / batch_size))
+
+
+ds_train = ds_train_orig.shuffle(buffer_size=len(y_train))
+ds_train = ds_train.repeat(count=None)
+ds_train = ds_train.batch(1)
+
+Ws, bs = [], []
+
+for i, batch in enumerate(ds_train):
+    if i >= steps_per_epoch * num_epochs:
+        break
+    Ws.append(model.w.numpy())
+    bs.append(model.b.numpy())
+
+    bx, by = batch
+    loss_val = loss_fn(model(bx), by)
+
+    train(model, bx, by, learning_rate=learning_rate)
+    if i%log_steps==0:
+        print('Epoch {:4d} Step {:2d} Loss {:6.4f}'.format(
+              int(i/steps_per_epoch), i, loss_val))
+
+print('Final Parameters:', model.w.numpy(), model.b.numpy())
+
+X_test = np.linspace(0, 9, num=100).reshape(-1, 1)
+X_test_norm = (X_test - np.mean(X_train)) / np.std(X_train)
+y_pred = model(tf.cast(X_test_norm, dtype=tf.float32))
+
+fig = plt.figure(figsize=(13, 5))
+ax = fig.add_subplot(1, 2, 1)
+plt.plot(X_train_norm, y_train, 'o', markersize=10)
+plt.plot(X_test_norm, y_pred, '--', lw=3)
+plt.legend(['Training examples', 'Linear Reg.'], fontsize=15)
+ax.set_xlabel('x', size=15)
+ax.set_ylabel('y', size=15)
+ax.tick_params(axis='both', which='major', labelsize=15)
+
+ax = fig.add_subplot(1, 2, 2)
+plt.plot(Ws, lw=3)
+plt.plot(bs, lw=3)
+plt.legend(['Weight w', 'Bias unit b'], fontsize=15)
+ax.set_xlabel('Iteration', size=15)
+ax.set_ylabel('Value', size=15)
+ax.tick_params(axis='both', which='major', labelsize=15)
+plt.show()
+
+
+# Model training via the .compile() and .fit() methods
+
+tf.random.set_seed(1)
+model = MyModel()
+
+model.compile(optimizer='sgd', 
+              loss=loss_fn,
+              metrics=['mae', 'mse'])
+
+model.fit(X_train_norm, y_train, 
+          epochs=num_epochs, batch_size=batch_size,
+          verbose=1)
+
+print(model.w.numpy(), model.b.numpy())
+
+
+X_test = np.linspace(0, 9, num=100).reshape(-1, 1)
+X_test_norm = (X_test - np.mean(X_train)) / np.std(X_train)
+
+y_pred = model(tf.cast(X_test_norm, dtype=tf.float32))
+
+fig = plt.figure(figsize=(13, 5))
+ax = fig.add_subplot(1, 2, 1)
+plt.plot(X_train_norm, y_train, 'o', markersize=10)
+plt.plot(X_test_norm, y_pred, '--', lw=3)
+plt.legend(['Training Samples', 'Linear Regression'], fontsize=15)
+
+ax = fig.add_subplot(1, 2, 2)
+plt.plot(Ws, lw=3)
+plt.plot(bs, lw=3)
+plt.legend(['W', 'bias'], fontsize=15)
+plt.show()
+
+# Building a multilayer perceptron for classifying flowers in the Iris dataset
+
+iris, iris_info = tfds.load('iris', with_info=True)
+
+print(iris_info)
+
+tf.random.set_seed(1)
+
+ds_orig = iris['train']
+ds_orig = ds_orig.shuffle(150, reshuffle_each_iteration=False)
+
+print(next(iter(ds_orig)))
+
+ds_train_orig = ds_orig.take(100)
+ds_test = ds_orig.skip(100)
+
+## checking the number of examples:
+
+n = 0
+for example in ds_train_orig:
+    n += 1
+print(n)
+
+
+n = 0
+for example in ds_test:
+    n += 1
+print(n)
+
+ds_train_orig = ds_train_orig.map(
+    lambda x: (x['features'], x['label']))
+
+ds_test = ds_test.map(
+    lambda x: (x['features'], x['label']))
+
+next(iter(ds_train_orig))
+
+iris_model = tf.keras.Sequential([
+    tf.keras.layers.Dense(16, activation='sigmoid', 
+                          name='fc1', input_shape=(4,)),
+    tf.keras.layers.Dense(3, name='fc2', activation='softmax')])
+
+iris_model.summary()
+
+iris_model.compile(optimizer='adam',
+                   loss='sparse_categorical_crossentropy',
+                   metrics=['accuracy'])
+
+num_epochs = 100
+training_size = 100
+batch_size = 2
+steps_per_epoch = np.ceil(training_size / batch_size)
+
+ds_train = ds_train_orig.shuffle(buffer_size=training_size)
+ds_train = ds_train.repeat()
+ds_train = ds_train.batch(batch_size=batch_size)
+ds_train = ds_train.prefetch(buffer_size=1000)
+
+history = iris_model.fit(ds_train, epochs=num_epochs,
+                         steps_per_epoch=steps_per_epoch, 
+                         verbose=0)
+
+hist = history.history
+
+fig = plt.figure(figsize=(12, 5))
+ax = fig.add_subplot(1, 2, 1)
+ax.plot(hist['loss'], lw=3)
+ax.set_title('Training loss', size=15)
+ax.set_xlabel('Epoch', size=15)
+ax.tick_params(axis='both', which='major', labelsize=15)
+
+ax = fig.add_subplot(1, 2, 2)
+ax.plot(hist['accuracy'], lw=3)
+ax.set_title('Training accuracy', size=15)
+ax.set_xlabel('Epoch', size=15)
+ax.tick_params(axis='both', which='major', labelsize=15)
+plt.tight_layout()
+plt.show()
+
+
+# Evaluating the trained model on the test dataset
+results = iris_model.evaluate(ds_test.batch(50), verbose=0)
+print('Test loss: {:.4f}   Test Acc.: {:.4f}'.format(*results))
+
+
+# Saving and reloading the trained model
+iris_model.save('iris-classifier.h5', 
+                overwrite=True,
+                include_optimizer=True,
+                save_format='h5')
+
+iris_model_new = tf.keras.models.load_model('iris-classifier.h5')
+
+iris_model_new.summary()
+
+results = iris_model_new.evaluate(ds_test.batch(50), verbose=0)
+print('Test loss: {:.4f}   Test Acc.: {:.4f}'.format(*results))
+
+labels_train = []
+for i,item in enumerate(ds_train_orig):
+    labels_train.append(item[1].numpy())
+    
+labels_test = []
+for i,item in enumerate(ds_test):
+    labels_test.append(item[1].numpy())
+print('Training Set: ',len(labels_train), 'Test Set: ', len(labels_test))
+
+iris_model_new.to_json()
