8fe23bddde07e1abd5dc38c9c9c36f354eb8df0d
Fabio Gaiera <fagaiera@gmail.com>
2020-05-24 11:27:55 +0200

    Chapter 04

---
+++ Chapter 04/05 Selecting Meaningful Features/program.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.neighbors import KNeighborsClassifier
+from sbs import SBS
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+df_wine = pd.read_csv('wine.data', header=None)
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium', 'Total phenols',
+                   'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins',
+                   'Color intensity', 'Hue', 'OD280/OD315 of diluted wines',
+                   'Proline']
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+
+X_train, X_test, y_train, y_test =train_test_split(X, y, test_size=0.3, random_state=0, stratify=y)
+
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
+
+# Selecting meaningful features
+
+# For regularized models in scikit-learn that support L1 regularization, we can simply set the `penalty` parameter to `'l1'` to obtain a sparse solution:
+
+LogisticRegression(penalty='l1', solver='liblinear', multi_class='ovr')
+
+
+# Applied to the standardized Wine data ...
+
+
+lr = LogisticRegression(penalty='l1', C=1.0, solver='liblinear', multi_class='ovr')
+# Note that C=1.0 is the default. You can increase
+# or decrease it to make the regulariztion effect
+# stronger or weaker, respectively.
+lr.fit(X_train_std, y_train)
+print('Training accuracy:', lr.score(X_train_std, y_train))
+print('Test accuracy:', lr.score(X_test_std, y_test))
+
+lr.intercept_
+np.set_printoptions(8)
+lr.coef_[lr.coef_!=0].shape
+lr.coef_
+
+fig = plt.figure()
+ax = plt.subplot(111)
+    
+colors = ['blue', 'green', 'red', 'cyan', 
+          'magenta', 'yellow', 'black', 
+          'pink', 'lightgreen', 'lightblue', 
+          'gray', 'indigo', 'orange']
+
+weights, params = [], []
+for c in np.arange(-4., 6.):
+    lr = LogisticRegression(penalty='l1', C=10.**c, solver='liblinear', 
+                            multi_class='ovr', random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+weights = np.array(weights)
+
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column],
+             label=df_wine.columns[column + 1],
+             color=color)
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10**(-5), 10**5])
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center', 
+          bbox_to_anchor=(1.38, 1.03),
+          ncol=1, fancybox=True)
+
+plt.savefig('image01.png', dpi=300, bbox_inches='tight', pad_inches=0.2)
+plt.show()
+
+
+
+# Sequential feature selection algorithms
+
+knn = KNeighborsClassifier(n_neighbors=5)
+
+# selecting features
+sbs = SBS(knn, k_features=1)
+sbs.fit(X_train_std, y_train)
+
+# plotting performance of feature subsets
+k_feat = [len(k) for k in sbs.subsets_]
+
+plt.plot(k_feat, sbs.scores_, marker='o')
+plt.ylim([0.7, 1.02])
+plt.ylabel('Accuracy')
+plt.xlabel('Number of features')
+plt.grid()
+plt.tight_layout()
+plt.savefig('image02.png', dpi=300)
+plt.show()
+
+k3 = list(sbs.subsets_[10])
+print(df_wine.columns[1:][k3])
+
+knn.fit(X_train_std, y_train)
+print('Training accuracy:', knn.score(X_train_std, y_train))
+print('Test accuracy:', knn.score(X_test_std, y_test))
+
+knn.fit(X_train_std[:, k3], y_train)
+print('Training accuracy:', knn.score(X_train_std[:, k3], y_train))
+print('Test accuracy:', knn.score(X_test_std[:, k3], y_test))
